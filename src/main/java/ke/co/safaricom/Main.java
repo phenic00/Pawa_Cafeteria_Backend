c708e1f3e76acf4a6f7372d8ca9fb74ec9141130
Violet <vnjuguna@gmail.com>
2023-08-02 04:53:59 +0300

    creating the landing page route to link index, added shared utils and added the index css

--- src/main/java/ke/co/safaricom/Main.java
+++ src/main/java/ke/co/safaricom/Main.java
@@ -1,7 +1,21 @@
 package ke.co.safaricom;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+
+
+import static spark.Spark.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        staticFileLocation("/public");
+
+        get("/",(request, response) ->{
+            return new ModelAndView( new HashMap<>(), "index.hbs");
+        },new HandlebarsTemplateEngine());
+
     }
 }
