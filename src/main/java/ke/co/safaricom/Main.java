008d3fa713d6901b3c68d5cfa24cf38ef1a848e4
Violet <vnjuguna@gmail.com>
2023-08-02 05:34:04 +0300

    add the route for landing page to main, add shared utils and css on the index file

--- src/main/java/ke/co/safaricom/Main.java
+++ src/main/java/ke/co/safaricom/Main.java
@@ -1,7 +1,17 @@
 package ke.co.safaricom;
 
+import spark.ModelAndView;
+
+import java.util.HashMap;
+
+import static spark.Spark.*;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        get("/",(request, response) ->{
+            return new ModelAndView( new HashMap<>(), "index.hbs");
+        },new HandlebarsTemplateEngine());
     }
 }
